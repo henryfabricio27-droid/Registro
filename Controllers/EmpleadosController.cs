2fc585f1cd08a7629e61981c8182d92117de2613
JONATHANESPINOSAISTER <jonathan.espinosa@ister.edu.ec>
2025-08-27 00:23:08 -0500

    Add files via upload

---
+++ Controllers/EmpleadosController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+
+namespace Registro.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            if (Session["usuario"] == null) return RedirectToAction("Ingreso", "Home");
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            if (Session["usuario"] == null) return RedirectToAction("Ingreso", "Home");
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            if (Session["usuario"] == null) return RedirectToAction("Ingreso", "Home");
+            return View();
+        }
+
+        public ActionResult Titulos()
+        {
+            if (Session["usuario"] == null) return RedirectToAction("Ingreso", "Home");
+            return View();
+        }
+
+        public ActionResult Salarios()
+        {
+            if (Session["usuario"] == null) return RedirectToAction("Ingreso", "Home");
+            return View();
+        }
+
+        public ActionResult Auditoria()
+        {
+            if (Session["usuario"] == null) return RedirectToAction("Ingreso", "Home");
+            return View();
+        }
+    }
+}
